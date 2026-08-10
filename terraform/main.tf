aa1905616a9af2da637b098f139e2108236aebad
teraoti <150186528+teraoti@users.noreply.github.com>
2026-08-10 10:08:47 +0100

    demo-vm deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,7 +46,6 @@ resource "azurerm_network_interface" "nic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
 
