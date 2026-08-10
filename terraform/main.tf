d361b7c7fe99c36303fdaa4975f6858f19e11789
teraoti <150186528+teraoti@users.noreply.github.com>
2026-08-10 10:14:33 +0100

    demo-vm deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -66,7 +66,6 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   os_disk {
     caching              = "ReadWrite"
-    storage_tier         = "Standard"
     storage_account_type = "Standard_LRS"
   }
 
